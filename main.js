4c50152fae7c1b2faa0578183689b48ce922a1dd
alfachri <xxdutt78@gmail.com>
2023-11-05 06:05:07 +0700

    membuat dark theme

--- main.js
+++ main.js
@@ -5,13 +5,31 @@ const closeMenu = document.querySelector(".btn-close");
 const toggleMenu = () => {
     menu.style.display = "flex";
     closeMenu.style.display = "flex";
+    icon.style.display = "none";
 }
 
 closeMenu.addEventListener("click", () => {
     menu.style.display = "none";
     closeMenu.style.display = "none";
+    icon.style.display = "flex";
 });
 
 // Dark Mode 
-const btnMode = document.querySelector(".mode");
+const root = document.querySelector(":root");
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", function(){
+    this.classList.toggle("bi-moon");
+    if(this.classList.toggle("bi-brightness-high-fill")) {
+        root.style.setProperty("--primary-dark", "#1c1c1c");
+        root.style.setProperty("--primary-light", "#f2f2f2");
+        root.style.setProperty("--primary-semi", "#5a5a5a");
+        root.style.transition = "1.5s";
+    } else {
+        root.style.setProperty("--primary-dark", "#f2f2f2");
+        root.style.setProperty("--primary-light", "#1c1c1c");
+        root.style.setProperty("--primary-semi", "#c7c7c7");
+        root.style.transition = "1.5s";
+    }
+})
 
